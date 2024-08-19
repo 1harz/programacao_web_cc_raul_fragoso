9bd34d8ac2839acf4d8b31e1eab26da367d2a4c1
1harz <raulfalluh@gmail.com>
2024-08-18 22:51:08 -0300

    Adicionando REGEX de validacao de email.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /*TODO -> REFATORAR O JS*/
 
-// Ao clicar em menu ele abre as opcoes
+// Validar email
+function validarEmail(email) {
+    // REGEX DO EMAIL - ACEITA QUALQUERCOISA@QUALQUERCOISA.COM - QUALQUER COISA NAO PODE SER ESPACO E NEM @
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+// Ao clicar em menu ele abre as opcoes do menu
 document.getElementById('menu-icon').addEventListener('click', function() {
     var menu = document.getElementById('menu-options');
     if (menu.style.display === 'flex') {
@@ -10,17 +17,27 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     }
 });
 
-// Controle do popup de login
+// Controla o popup de login
 document.getElementById('login-btn').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('login-popup').style.display = 'flex';
+});
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    if (validarEmail(email)) {
+        alert('Email valido'); //Gera alerta falando que o email inserido esta ok
+    } else {
+        alert('Por favor, insira um email valido.');
+    }
 });
 
 document.getElementById('close-popup').addEventListener('click', function() {
     document.getElementById('login-popup').style.display = 'none';
 });
 
-// Fechar popup caso clicar fora da regiao dela
+// Fechar o popup caso clicar fora da regiao dele
 window.addEventListener('click', function(event) {
     var popup = document.getElementById('login-popup');
     if (event.target === popup) {
